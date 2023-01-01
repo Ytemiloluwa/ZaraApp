1b77545d4f2893f525628c9025f573c92834352c
Temi <ytemiloluwa@gmail.com>
2023-01-01 09:58:28 +0100

    [ Chore :  Added Color+ ]

---
+++ ZaraApp/Extensions/Color+.swift
@@ -0,0 +1,15 @@
+//
+//  Color+.swift
+//  ZaraApp
+//
+//  Created by Temiloluwa on 01/01/2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let background = Color("background")
+    static let text = Color("text")
+}
