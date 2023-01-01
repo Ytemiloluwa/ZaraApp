ccd1824b171ee56c435b522373f2c571592bbd0e
Temi <ytemiloluwa@gmail.com>
2023-01-01 01:07:13 +0100

    Initial Commit

---
+++ ZaraApp/ZaraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZaraAppApp.swift
+//  ZaraApp
+//
+//  Created by Temiloluwa on 01/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ZaraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
