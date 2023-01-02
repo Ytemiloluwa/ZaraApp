605b8d6e81c0c4b729f4a2e85f2b7421d70b7485
Temi <ytemiloluwa@gmail.com>
2023-01-03 00:50:31 +0100

    [ Chore : Added VerticalPageIndicator ]

---
+++ ZaraApp/Views/VerticalPageIndicator.swift
@@ -0,0 +1,41 @@
+//
+//  VerticalPageIndicator.swift
+//  ZaraApp
+//
+//  Created by Temiloluwa on 03/01/2023.
+//
+
+import SwiftUI
+
+struct VerticalPageIndicator: View {
+    
+    var numberOfPages: Int = 5
+    
+    @Binding var selectedPageIndex: Int?
+    var body: some View {
+        
+        VStack {
+            
+            ForEach(0..<numberOfPages) { i in
+                
+                if i == selectedPageIndex {
+                    
+                    Circle()
+                        .strokeBorder(lineWidth: 1.0, antialiased: true)
+                        .frame(width: 12, height: 12, alignment: .center)
+                }else {
+                    
+                    Circle()
+                        .frame(width: 7, height: 7, alignment: .center)
+                }
+            }
+        }.textColor()
+        
+    }
+}
+
+struct VerticalPageIndicator_Previews: PreviewProvider {
+    static var previews: some View {
+        VerticalPageIndicator(selectedPageIndex: .constant(0))
+    }
+}
