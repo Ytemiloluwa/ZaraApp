020837a885f38ee8e1d8608fc27839f563cfc91a
Temi <ytemiloluwa@gmail.com>
2023-01-02 09:52:40 +0100

    [ Chore : Added Tab ]

---
+++ ZaraApp/Models/Tab.swift
@@ -0,0 +1,17 @@
+//
+//  Tab.swift
+//  ZaraApp
+//
+//  Created by Temiloluwa on 02/01/2023.
+//
+
+import Foundation
+
+enum Tab {
+
+    case search
+    case bookmark
+    case menu
+    case profile
+    case cart
+}
