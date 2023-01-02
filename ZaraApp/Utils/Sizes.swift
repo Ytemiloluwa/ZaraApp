39d65b08c98c2ccdf7f118b502c7cc51217393a4
Temi <ytemiloluwa@gmail.com>
2023-01-03 00:26:47 +0100

    [ Chore : Added Sizes ]

---
+++ ZaraApp/Utils/Sizes.swift
@@ -0,0 +1,17 @@
+//
+//  Sizes.swift
+//  ZaraApp
+//
+//  Created by Temiloluwa on 03/01/2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct Sizes {
+    
+    static var navBarHeight: CGFloat { 50 }
+    static var tabHeight: CGFloat { UIScreen.main.bounds.width * 0.15}
+    static var screenWidth = UIScreen.main.bounds.width
+    static var screenHeight = UIScreen.main.bounds.height
+}
