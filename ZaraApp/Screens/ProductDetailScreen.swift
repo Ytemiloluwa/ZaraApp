b376161e4f53922189d286ed4199d72caaadfa09
Temi <ytemiloluwa@gmail.com>
2023-01-05 17:01:08 +0100

    [ Chore : Updated ProductDetailScreen ]

--- ZaraApp/Screens/ProductDetailScreen.swift
+++ ZaraApp/Screens/ProductDetailScreen.swift
@@ -18,19 +18,58 @@ struct ProductDetailScreen: View {
     
     @Namespace private var animation
     
-    private let foldedHeight: CGFloat = 150
+    private let foldedHeight: CGFloat = 300
     private let expandedHeight = Sizes.screenHeight - UIScreen.main.bounds.height * 0.22
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack(alignment: .top) {
+            
+            createPagingController()
+            
+            if selectedIndex == nil {
+                createCustomSheet()
+            } else {
+                createProductImageView()
+            }
+            
+            createNavBarView()
+        }.edgesIgnoringSafeArea(.all)
+            .animation(.linear, value: selectedIndex)
     }
     
     fileprivate func createCustomSheet() -> some View {
         
         CustomSheet(progress: self.$progress, foldedHeight: foldedHeight, expandedHeight: expandedHeight) {
             
-           ProductDetailContent(product: product)
+            ProductDetailContent(product: product)
         }
     }
+    
+    fileprivate func createProductImageView() -> ProductImageView {
+        let images = product.images
+        return ProductImageView(imageUrl: images[selectedIndex!], animation: animation) {
+            
+            
+            self.selectedIndex = nil
+            
+        }
+    }
+    
+    fileprivate func createPagingController() -> some View {
+        PageController(viewControllers:
+                        product.images.map({
+            UIHostingController(rootView:
+                                    ImagePageView(image: $0)
+                .matchedGeometryEffect(id: $0, in: animation)
+            )}), selectedIndex: self.$selectedIndex)
+    }
+    
+    fileprivate func createNavBarView() -> some View {
+        ClearNavBar(opacity: Double(1 - progress), onDismiss: onDismiss)
+            .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+    }
+    
+    
 }
 
 struct ProductDetailScreen_Previews: PreviewProvider {
